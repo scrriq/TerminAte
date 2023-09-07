bd34c2188d77b3275eb3ae89ec84f8a3be50e207
Scrriq <gdien.hc@gmail.com>
2023-09-07 22:38:26 +0200

    Sidebar Development Process *A prototype of the sidebar was made *Added new icons to the project

--- firp/src/components/AppMenu.vue
+++ firp/src/components/AppMenu.vue
@@ -4,13 +4,29 @@
             <div class="menu__logo-block">
                 <a class = 'menu__logo' href=""> <span class="text__orange">ENL</span>INO</a>
             </div>
-            <div class="menu__nav-items">
-                <div class="manu__nav-item">A</div>
-                <div class="manu__nav-item">B</div>
-                <div class="manu__nav-item">C</div>
-                <div class="manu__nav-item">D</div>
-                <div class="manu__nav-item">E</div>
-                <div class="manu__nav-item">F</div>
+            <div class="menu__nav-blocks">
+                <div class="menu__nav-block__switcher">
+                    <div class="manu__nav-item shedule">
+                        <span>Shedule</span> 
+                    </div>
+                    <div class="manu__nav-item card">
+                        <span>Card</span>
+                    </div>
+                    <div class="manu__nav-item memorise">
+                        <span>Memorise</span>
+                    </div>
+                    <div class="manu__nav-item dictionary">
+                        <span>Dictionary</span>
+                    </div>
+                </div>
+                <div class="menu__naw-block__addition">
+                    <div class="manu__nav-item setting">
+                        <span>Setting</span>
+                    </div>
+                    <div class="manu__nav-item account">
+                        <span>Account</span>
+                    </div>
+                </div>
             </div>
         </nav>
     </div>
@@ -24,14 +40,14 @@ export default {
 
 <style scoped>
 
-.menu{
+
+.menu {
     position: fixed;
     display: fixed;
     height: 100%;
     width: 225px;
-    background-color: black;
+    background-color: #012122;
     border-right: 1px solid white;
-
 }
 .menu__navigation {
     width: 100%;
@@ -40,20 +56,13 @@ export default {
     flex-direction: column;
     flex-direction: space-around;
 }
-.menu__nav-items{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: space-around;
-}
-.menu__logo-block{
+.menu__logo-block {
+    margin: 30px 0px 0px 0px;
     width: 100%;
     height: 65px;
-    background-color: rgb(71, 2, 46);
+    /* background-color: rgb(71, 2, 46); */
 
-    font-size: 42px;
+    font-size: 40px;
     font-family: 'Roboto Condensed';
     font-weight: 700;
     letter-spacing: 0.1em;
@@ -62,12 +71,115 @@ export default {
     justify-content: center;
 }
 .menu__logo {
-
 }
-.manu__nav-item{
-    font-size: 40px;
-}
-.text__orange{
+.text__orange {
     color: #FFB800 !important;
+}
+.menu__nav-blocks {
+    width: 100%;
+    height: 100%;
+    /* background-color: rgb(57, 57, 57); */
+    display: grid;
+    grid-template: 2fr 1fr/1fr;
+}
+.menu__nav-block__switcher {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    row-gap: 35px;
+}
+.menu__naw-block__addition {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    row-gap: 35px;
+}
+.manu__nav-item {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+    height: 60px;
+    padding: 0px 0px 0px 40px;
+    /* background-color: #210008; */
+    border: 1px solid black;
+}
+/* .manu__nav-item > span::before{
+    content: "";
+    position: relative;
+    float: left;
+    width: 25px;
+    height: 25px;
+    background: url('@/assets/icon/shedule.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: -2px 30px 0px 0px;
+} */
+.shedule > span::before{
+    content: "";
+    position: relative;
+    float: left;
+    width: 25px;
+    height: 25px;
+    background: url('@/assets/icon/shedule.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: -2px 30px 0px 0px;
+}
+.card > span::before{
+    content: "";
+    position: relative;
+    float: left;
+    width: 25px;
+    height: 25px;
+    background: url('@/assets/icon/card.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: -2px 30px 0px 0px;
+}
+.memorise > span::before{
+    content: "";
+    position: relative;
+    float: left;
+    width: 25px;
+    height: 25px;
+    background: url('@/assets/icon/memorise.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: -2px 30px 0px 0px;
+}
+.dictionary > span::before{
+    content: "";
+    position: relative;
+    float: left;
+    width: 25px;
+    height: 25px;
+    background: url('@/assets/icon/dictionary.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: -1px 30px 0px 0px;
+}
+.setting > span::before{
+    content: "";
+    position: relative;
+    float: left;
+    width: 25px;
+    height: 25px;
+    background: url('@/assets/icon/setting.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: -2px 30px 0px 0px;
+}
+.account > span::before{
+    content: "";
+    position: relative;
+    float: left;
+    width: 25px;
+    height: 25px;
+    background: url('@/assets/icon/account.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+    margin: -2px 30px 0px 0px;
 }
 </style>
