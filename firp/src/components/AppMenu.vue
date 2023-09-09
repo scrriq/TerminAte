86d9230c8015052ad9e74be42813b6e8d087b134
Scrriq <gdien.hc@gmail.com>
2023-09-10 00:45:40 +0200

    Sidebar Development *completely redesigned the concept of displaying icons *redesigned the concept of the sidebar logo *start developing the disappearing sidebar feature

--- firp/src/components/AppMenu.vue
+++ firp/src/components/AppMenu.vue
@@ -2,29 +2,65 @@
     <div class="menu">
         <nav class="menu__navigation">
             <div class="menu__logo-block">
-                <a class = 'menu__logo' href=""> <span class="text__orange">ENL</span>INO</a>
+                <div class="menu__logo">
+                    <img src="@/assets/icon/shedule.png" alt="">
+                </div>
+                <div class="menu__logo-text">
+                    <a class = 'menu__logo-link' href=""> <span v-if="show" class="text__orange">Language </span><br><span v-if='show'>Learning</span></a>
+                </div>
             </div>
             <div class="menu__nav-blocks">
                 <div class="menu__nav-block__switcher">
-                    <div class="manu__nav-item shedule">
-                        <span>Shedule</span> 
+                    <button class = 'test' @click="hello"> TEST </button>
+                    <div class="manu__nav-item">
+                        <div class="item__block-img">
+                            <img src="@/assets/icon/shedule.png" alt="">
+                        </div>
+                        <div class="item__block-text">
+                           <p v-if="show">Shedule</p> 
+                        </div>
                     </div>
-                    <div class="manu__nav-item card">
-                        <span>Card</span>
+                    <div class="manu__nav-item">
+                        <div class="item__block-img">
+                            <img src="@/assets/icon/card.png" alt="">
+                        </div>
+                        <div class="item__block-text">
+                           <p v-if="show">Card</p> 
+                        </div>
                     </div>
-                    <div class="manu__nav-item memorise">
-                        <span>Memorise</span>
+                    <div class="manu__nav-item">
+                        <div class="item__block-img">
+                            <img src="@/assets/icon/memorise.png" alt="">
+                        </div>
+                        <div class="item__block-text">
+                           <p v-if="show">Memorise</p> 
+                        </div>
                     </div>
-                    <div class="manu__nav-item dictionary">
-                        <span>Dictionary</span>
+                    <div class="manu__nav-item">
+                        <div class="item__block-img">
+                            <img src="@/assets/icon/dictionary.png" alt="">
+                        </div>
+                        <div class="item__block-text">
+                           <p v-if="show">Dictionary</p> 
+                        </div>
                     </div>
                 </div>
                 <div class="menu__naw-block__addition">
-                    <div class="manu__nav-item setting">
-                        <span>Setting</span>
+                    <div class="manu__nav-item">
+                        <div class="item__block-img">
+                            <img src="@/assets/icon/setting.png" alt="">
+                        </div>
+                        <div class="item__block-text">
+                           <p v-if="show">Setting</p> 
+                        </div>
                     </div>
-                    <div class="manu__nav-item account">
-                        <span>Account</span>
+                    <div class="manu__nav-item">
+                        <div class="item__block-img">
+                            <img src="@/assets/icon/account.png" alt="">
+                        </div>
+                        <div class="item__block-text">
+                           <p v-if="show">Account</p> 
+                        </div>
                     </div>
                 </div>
             </div>
@@ -34,7 +70,16 @@
 
 <script>
 export default {
-
+    data(){
+        return{
+            show: true
+        }
+    },
+    methods: {
+        hello(){
+            this.show = !this.show
+        }
+    },
 }
 </script>
 
@@ -56,21 +101,41 @@ export default {
     flex-direction: column;
     flex-direction: space-around;
 }
+
 .menu__logo-block {
+    display: grid;
+    grid-template-columns: 1fr 1.8fr;
     margin: 30px 0px 0px 0px;
     width: 100%;
     height: 65px;
-    /* background-color: rgb(71, 2, 46); */
-
-    font-size: 40px;
+    gap: 0px 20px;
+}
+.menu__logo {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+}
+.menu__logo > img{
+    position: relative;
+    float: left;
+    width: 55px;
+    height: 55px;
+    background: url('@/assets/icon/shedule.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+.menu__logo-text {
+    font-size: 22px;
     font-family: 'Roboto Condensed';
     font-weight: 700;
     letter-spacing: 0.1em;
     display: flex;  
     align-items: center;
-    justify-content: center;
+    justify-content: flex-start;
 }
-.menu__logo {
+.menu__logo-link {
 }
 .text__orange {
     color: #FFB800 !important;
@@ -78,7 +143,6 @@ export default {
 .menu__nav-blocks {
     width: 100%;
     height: 100%;
-    /* background-color: rgb(57, 57, 57); */
     display: grid;
     grid-template: 2fr 1fr/1fr;
 }
@@ -97,89 +161,36 @@ export default {
     row-gap: 35px;
 }
 .manu__nav-item {
+    display: grid;
+    grid-template-columns: 0.8fr 2fr;
+    gap: 0px 25px;
+}
+
+.item__block-img {
+    display: flex;
+    justify-content: flex-end;
+    width: 100%;
+    height: 100%;
+}
+
+.item__block-img > img{
+    position: relative;
+    float: left;
+    width: 30px;
+    height: 30px;
+    background: url('@/assets/icon/shedule.png');
+    background-size: contain;
+    background-repeat: no-repeat;
+}
+.item__block-text {
     display: flex;
     align-items: center;
-    font-size: 20px;
-    height: 60px;
-    padding: 0px 0px 0px 40px;
-    /* background-color: #210008; */
-    border: 1px solid black;
+    justify-content: flex-start;
+    width: 100%;
+    height: 100%;
+    font-size: 24px;
 }
-/* .manu__nav-item > span::before{
-    content: "";
-    position: relative;
-    float: left;
-    width: 25px;
-    height: 25px;
-    background: url('@/assets/icon/shedule.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin: -2px 30px 0px 0px;
-} */
-.shedule > span::before{
-    content: "";
-    position: relative;
-    float: left;
-    width: 25px;
-    height: 25px;
-    background: url('@/assets/icon/shedule.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin: -2px 30px 0px 0px;
-}
-.card > span::before{
-    content: "";
-    position: relative;
-    float: left;
-    width: 25px;
-    height: 25px;
-    background: url('@/assets/icon/card.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin: -2px 30px 0px 0px;
-}
-.memorise > span::before{
-    content: "";
-    position: relative;
-    float: left;
-    width: 25px;
-    height: 25px;
-    background: url('@/assets/icon/memorise.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin: -2px 30px 0px 0px;
-}
-.dictionary > span::before{
-    content: "";
-    position: relative;
-    float: left;
-    width: 25px;
-    height: 25px;
-    background: url('@/assets/icon/dictionary.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin: -1px 30px 0px 0px;
-}
-.setting > span::before{
-    content: "";
-    position: relative;
-    float: left;
-    width: 25px;
-    height: 25px;
-    background: url('@/assets/icon/setting.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin: -2px 30px 0px 0px;
-}
-.account > span::before{
-    content: "";
-    position: relative;
-    float: left;
-    width: 25px;
-    height: 25px;
-    background: url('@/assets/icon/account.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-    margin: -2px 30px 0px 0px;
+.test{
+    color:black;
 }
 </style>
--- firp/src/components/AppPage.vue
+++ firp/src/components/AppPage.vue
@@ -21,4 +21,5 @@ export default {
 }
 .marin__app-page__container {
 }
+
 </style>
