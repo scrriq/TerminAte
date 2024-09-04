7ed2784394c5889ca1dbcd5d03291d51f86c39e0
scrriq <gdien.hc@gmail.com>
2024-09-05 00:40:05 +0200

    The menu has been optimized *Added a menu item component *The menu layout has been replaced by the reuse of the menu item component *The style has been changed ( it will be further developed ) *Added an array with information for cards

--- firp/src/components/AppMenu.vue
+++ firp/src/components/AppMenu.vue
@@ -1,70 +1,33 @@
 <template>
     <div class="menu" v-bind:class="{menuShow: !show}" @mouseenter="show = true" @mouseleave="show = false" >
-        <nav class="menu__navigation">
-            <div class="menu__double-shell">
-                <div class="menu__first-shell">
-                    <div class="menu__logo-block">
-                        <img src="@/assets/icon/shedule.png" alt="">
-                    </div>
-                    <div class="menu__icon-blocks default__nav-side">
-                        <div class="icon__block-img menu__default-block">
-                            <img src="@/assets/icon/shedule.png" alt="">
-                        </div>
-                        <div class="icon__block-img menu__default-block">
-                            <img src="@/assets/icon/card.png" alt="">
-                        </div>
-                        <div class="icon__block-img menu__default-block">
-                            <img src="@/assets/icon/memorise.png" alt="">
-                        </div>
-                        <div class="icon__block-img menu__default-block forward__menu-setting">
-                            <img src="@/assets/icon/dictionary.png" alt="">
-                        </div>
-                        <div class="icon__block-img menu__default-block">
-                            <img src="@/assets/icon/setting.png" alt="">
-                        </div>
-                        <div class="icon__block-img menu__default-block">
-                            <img src="@/assets/icon/account.png" alt="">
-                        </div>
-                    </div>
+        <div class="menu__container">
+            <nav class="menu__navigation">
+                <div class="menu__header">
+                    <h1>Hello</h1>                    
                 </div>
-                <div class="menu__second-shell" v-bind:class="{switchBlock: !show}">
-                    <div class="menu__logo-block menu_logo__text-block">
-                        <a class = 'menu__logo-link' href=""> 
-                            <p class="text__orange">Language Learning</p>
-                        </a>
-                    </div>
-                    <div class="menu__text-blocks default__nav-side">
-                            <div class="item__text-block menu__default-block">
-                                <p>Shedule</p>
-                            </div>
-                            <div class="item__text-block menu__default-block">
-                                <p>Card</p>
-                            </div>
-                            <div class="item__text-block menu__default-block">
-                                <p>Memorise</p> 
-                            </div>
-                            <div class="item__text-block menu__default-block forward__menu-setting">
-                                <p>Dictionary</p> 
-                            </div>
-                            <div class="item__text-block menu__default-block">
-                                <p>Setting</p> 
-                            </div>
-                            <div class="item__text-block menu__default-block">
-                                <p>Account</p> 
-                            </div>
-                    </div>
+                <div class="menu__navigation__shell">
+                    <menu-item class="menu__items" v-for="card in cards" :card = "card" :key="card" :show="show" ></menu-item>
                 </div>
-
-            </div>
-        </nav>
+            </nav>
+        </div>
     </div>
 </template>
 
-<script>
+
+
+<script>    
+import MenuItem from './MenuItem.vue'
 export default {
     data(){
         return{
-            show: false,
+            show: true,
+            cards:[
+            {name: 'Dictionary', imageLink: require("../assets/icon/dictionary.png")},
+            {name: 'Shedule',imageLink: require("../assets/icon/shedule.png")},
+            {name: 'Card', imageLink: require("../assets/icon/card.png")},
+            {name: 'Memorise', imageLink: require("../assets/icon/memorise.png")},
+            {name: 'Account', imageLink: require("../assets/icon/account.png")},
+            ]
         }
     },
     methods: {
@@ -72,6 +35,9 @@ export default {
             this.show = !this.show
         }
     },
+    components:{
+        MenuItem
+    }
 }
 </script>
 
@@ -96,121 +62,31 @@ export default {
     height: 100%;
     display: flex;
     flex-direction: column;
-    justify-content: space-around;
 }
 
-
-
-.menu__double-shell {
+.menu__container{
     width: 100%;
     height: 100%;
+    padding: 10px;
+
+}
+
+.menu__header{
     display: flex;
-}
-.menu__first-shell {
-    height: 100%;
-    min-width: 90px;
-    max-width: 90px;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-}
-.menu__second-shell {
-    height: 100%;
-    width: 100%;
-    overflow: hidden;
-}
-.switchBlock{
-}
-
-.menu__logo-block{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    min-height: 55px;
-    max-height: 55px;
-    margin: 30px 0px 0px 0px;
-}
-.menu__logo-block > img{
-    position: relative;
-    float: left;
-    width: 55px;
-    height: 55px;
-    background: url('@/assets/icon/shedule.png');
-    background-size: contain;
-    background-repeat: no-repeat;
-}
-.menu_logo__text-block{
 
 }
-.menu__logo-link{
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
+
+.menu__navigation__shell{
     width: 100%;
     height: 100%;
-    font-size: 22px;
-    font-family: 'Roboto Condensed';
-    font-weight: 700;
-    letter-spacing: 0.1em;
-    padding: 0px 0px 0px 10px;
+    display: grid;
+    grid-template: 2fr 2fr 2fr 2fr 5fr / 1fr;
+    align-items: flex-end;
 }
 
-
-.menu__icon-blocks {
+.menu__items{
     width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-.icon__block-img {
-    display: flex;
-    width: 100%;
-    height: 100%;
-}
-.icon__block-img {
-    width: 35px;
-    height: 35px;
-    position: relative;
-}
-.icon__block-img > img{
-    position: absolute;
-    top:0;
-    left:0;
-    width: 100%;
-    height: 100%;
-}
-.menu__text-blocks{
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-.item__text-block{
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    padding: 0px 0px 0px 10px;
-    width: 100%;
-    height: 100%;
-    font-size: 22px;
-}
-
-.menu__default-block{
-    min-height: 35px;
-    max-height: 35px;
-    margin: 0px 0px 40px 0px;
-}
-.forward__menu-setting{
-    margin: 0px 0px 135px;
-}
-.default__nav-side{
-    margin: 90px 0px 0px 0px;
-}
-
-.text__orange {
-    color: #FFB800 !important;
+    height: 75px;
 }
 
 
---
+++ firp/src/components/MenuItem.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="menu__item">  
+        <div class="menu__item__container">
+            <button class="menu__item__button" href="">
+            <!-- <button class="menu__item__button" :class="{menu__item__button__temp: !show}" > -->
+                <div class="menu__item__icon" src="/firp/src/assets/icon/dictionary.png">
+                    <img :src="card.imageLink" class ="menu__icon__image">
+                </div>
+                <transition name="animation__menu">
+                    <div class="menu__item__title" v-show="show" >
+                        {{ card.name }}
+                    </div>
+                </transition>
+            </button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default{
+    props:{
+        card:{
+            type: Object,
+            required: true,
+        },
+        show: Boolean,
+    }
+
+    
+
+}
+
+</script>
+
+
+
+<style>
+.menu__item{
+    width: 100%;
+    height: 100%;
+
+}
+
+.menu__item__container{
+    width: 100%;
+    height: 100%;
+}
+
+.menu__item__button{
+    width: 100%;
+    height: 100%;
+    display: grid;
+    grid-template: 1fr/ 1fr 2.5fr;
+    border: 0px;
+    background: none;
+}
+
+.menu__item__icon{
+    width: 100%;
+    height: 100%;
+    padding: 18px 10px;
+}
+.menu__icon__image{
+    width: 35px;
+    height: 35px;
+    top: 0;
+    left: 0;
+}
+
+.menu__item__title{
+    width: 100%;
+    height: 100%;
+    font-size: 20px;
+    font-family:'Courier New', Courier, monospace;
+    font-weight: 500;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 0px 0px 0px 8px;
+}
+
+.animation__menu-leave-to {
+  opacity: 0;
+}
+.animation__menu-enter-active{
+  transition: opacity 0.9s ease;
+}
+
+.animation__menu-enter-from{
+  opacity: 0;
+}
+</style>
